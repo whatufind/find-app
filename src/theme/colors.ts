effb8832c42f1bd24ab77fa81f57f7f13f0b99a7
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2024-10-28 23:25:59 +0600

    add restyle add theme and do project structure clean

---
+++ src/theme/colors.ts
@@ -0,0 +1,190 @@
+const palette = {
+    // bg color hex
+    color1: '#f5f5f5',
+    color2: '#FFFFFF',
+
+    // primary colors hex
+    color3: '#F2FDF9',
+    color4: '#CBDFD8',
+    color5: '#B2CFC6',
+    color6: '#8EB9AB',
+    color7: '#79AC9B',
+    color8: '#579782',
+    color9: '#4F8976',
+    color10: '#3E6B5C',
+    color11: '#305348',
+    color12: '#253F37',
+
+    // secondary colors hex
+    color13: '#EBEDEF',
+    color14: '#C0C7CD',
+    color15: '#A2ACB5',
+    color16: '#778694',
+    color17: '#5D6E7F',
+    color18: '#344A5F',
+    color19: '#2F4356',
+    color20: '#253543',
+    color21: '#1D2934',
+    color22: '#161F28',
+
+    // black colors hex
+    color23: '#E7E8E7',
+    color24: '#B4B6B6',
+    color25: '#8F9392',
+    color26: '#5C6260',
+    color27: '#3D4441',
+    color28: '#0C1512',
+    color29: '#0B1310',
+    color30: '#090F0D',
+    color31: '#070C0A',
+    color32: '#050908',
+
+    // success colors hex
+    color33: '#E8F8F1',
+    color34: '#B9EBD3',
+    color35: '#97E1BD',
+    color36: '#68D39F',
+    color37: '#4ACA8D',
+    color38: '#1DBD70',
+    color39: '#1AAC66',
+    color40: '#158650',
+    color41: '#10683E',
+    color42: '#0C4F2F',
+
+    // warning colors hex
+    color43: '#FEF9F4',
+    color44: '#FCEEDD',
+    color45: '#FAE5CC',
+    color46: '#F8D9B5',
+    color47: '#F6D2A6',
+    color48: '#F4C790',
+    color49: '#DEB583',
+    color50: '#AD8D66',
+    color51: '#866D4F',
+    color52: '#66543C',
+
+    // danger colors hex
+    color53: '#FCEFF1',
+    color54: '#F7CED2',
+    color55: '#F3B7BD',
+    color56: '#ED969F',
+    color57: '#E9818C',
+    color58: '#E4626F',
+    color59: '#CF5965',
+    color60: '#A2464F',
+    color61: '#7F4C48',
+    color62: '#7D363D',
+
+    color63: 'transparent',
+
+    // neutral colors hex
+    color64: '#EBEBEB',
+    color65: '#C0C0C0',
+    color66: '#A1A1A1',
+    color67: '#767676',
+    color68: '#5C5C5C',
+    color69: '#333333',
+    color70: '#2E2E2E',
+    color71: '#242424',
+    color72: '#1C1C1C',
+    color73: '#151515',
+  };
+
+  export const colors = {
+    // bg color
+    background: palette.color1,
+    white: palette.color2,
+    transparent: palette.color63,
+    overlay: palette.color63, // need to add into design system
+
+    // primary colors
+    primary: palette.color8,
+    primary50: palette.color3,
+    primary100: palette.color4,
+    primary200: palette.color5,
+    primary300: palette.color6,
+    primary400: palette.color7,
+    primary500: palette.color8,
+    primary600: palette.color9,
+    primary700: palette.color10,
+    primary800: palette.color11,
+    primary900: palette.color12,
+
+    // secondary colors
+    secondary: palette.color18,
+    secondary50: palette.color13,
+    secondary100: palette.color14,
+    secondary200: palette.color15,
+    secondary300: palette.color16,
+    secondary400: palette.color17,
+    secondary500: palette.color18,
+    secondary600: palette.color19,
+    secondary700: palette.color20,
+    secondary800: palette.color21,
+    secondary900: palette.color22,
+
+    // black colors
+    black: palette.color28,
+    black50: palette.color23,
+    black100: palette.color24,
+    black200: palette.color25,
+    black300: palette.color26,
+    black400: palette.color27,
+    black500: palette.color28,
+    black600: palette.color29,
+    black700: palette.color30,
+    black800: palette.color31,
+    black900: palette.color32,
+
+    // success colors
+    success: palette.color38,
+    success50: palette.color33,
+    success100: palette.color34,
+    success200: palette.color35,
+    success300: palette.color36,
+    success400: palette.color37,
+    success500: palette.color38,
+    success600: palette.color39,
+    success700: palette.color40,
+    success800: palette.color41,
+    success900: palette.color42,
+
+    // warning colors
+    warning: palette.color48,
+    warning50: palette.color43,
+    warning100: palette.color44,
+    warning200: palette.color45,
+    warning300: palette.color46,
+    warning400: palette.color47,
+    warning500: palette.color48,
+    warning600: palette.color49,
+    warning700: palette.color50,
+    warning800: palette.color51,
+    warning900: palette.color52,
+
+    // danger colors
+    danger: palette.color58,
+    danger50: palette.color53,
+    danger100: palette.color54,
+    danger200: palette.color55,
+    danger300: palette.color56,
+    danger400: palette.color57,
+    danger500: palette.color58,
+    danger600: palette.color59,
+    danger700: palette.color60,
+    danger800: palette.color61,
+    danger900: palette.color62,
+
+    // nurtral colors
+    neutral: palette.color69,
+    neutral50: palette.color64,
+    neutral100: palette.color65,
+    neutral200: palette.color66,
+    neutral300: palette.color67,
+    neutral400: palette.color68,
+    neutral500: palette.color69,
+    neutral600: palette.color70,
+    neutral70: palette.color71,
+    neutral800: palette.color72,
+    neutral900: palette.color73,
+  };
