f062b48d847638b22292d03b9f0fd55dbd61267f
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2024-10-29 22:48:54 +0600

    update project name

--- ios/rnboilerplateTests/rnboilerplateTests.m
+++ ios/rnboilerplateTests/rnboilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface whatufindTests : XCTestCase
+@interface rnboilerplateTests : XCTestCase
 
 @end
 
-@implementation whatufindTests
+@implementation rnboilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
