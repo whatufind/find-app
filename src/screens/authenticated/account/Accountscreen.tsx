0cc05ec46a611ce4edf2fefeb73806cff7bea833
sara.231.pc <sara.231.pc@gmail.com>
2025-04-17 17:13:00 +0600

    implement the request card

--- src/screens/authenticated/account/Accountscreen.tsx
+++ src/screens/authenticated/account/Accountscreen.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Card,
   Center,
+  Clickable,
   ContentSafeAreaView,
   FastImage,
   Header,
@@ -32,6 +33,7 @@ import {FlashList} from '@shopify/flash-list';
 import React, {useCallback} from 'react';
 import {ImageBackground, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
+import RequestCard from '../request/RequestCard';
 
 const AccountHeader = ({user}) => {
   const navigation = useNavigation();
@@ -58,58 +60,63 @@ const ProfileSection = ({user}) => {
     <ImageBackground
       source={{uri: getImageUrl(user?.coverPhoto)}}
       style={styles.imageBanner}>
-      <Box width="100%" height="100%"  bg="backdrop" alignItems="center" justifyContent="center">
-      <Center>
-        <Box position="absolute" zIndex={10} top={0} right={-10}>
-          <IconButton
-            onPress={() => navigation.navigate('Manage Account')}
-            size={10}
-            icon="pencil-circle"
-            variant="vector"
-            type="materialCommunity"
+      <Box
+        width="100%"
+        height="100%"
+        bg="backdrop"
+        alignItems="center"
+        justifyContent="center">
+        <Center>
+          <Box position="absolute" zIndex={10} top={0} right={-10}>
+            <IconButton
+              onPress={() => navigation.navigate('Manage Account')}
+              size={10}
+              icon="pencil-circle"
+              variant="vector"
+              type="materialCommunity"
+            />
+          </Box>
+          <FastImage
+            borderRadius="rounded-full"
+            width={100}
+            height={100}
+            source={{uri: getImageUrl(user?.profilePicture)}}
           />
-        </Box>
-        <FastImage
-          borderRadius="rounded-full"
-          width={100}
-          height={100}
-          source={{uri: getImageUrl(user?.profilePicture)}}
-        />
-       <Text textAlign="center" variant="heading3" color="white">
-          {user?.name}
-        </Text>
-        <Text textAlign="center" variant="b4regular" color="white">
-          {user?.professions?.[0]?.name}
-        </Text>
-      </Center>
+          <Text textAlign="center" variant="heading3" color="white">
+            {user?.name}
+          </Text>
+          <Text textAlign="center" variant="b4regular" color="white">
+            {user?.professions?.[0]?.name}
+          </Text>
+        </Center>
       </Box>
     </ImageBackground>
   );
 };
 
-const StatsCard = ({value, label}) => (
-  <Card
-    variant="outlined"
+const StatsCard = ({value, label, onPress, isSelected}) => (
+  <Clickable
+    onPress={onPress}
     borderRadius="rounded-full"
-    flexDirection="row"
+    bg={isSelected ? 'primary' : 'white'}
     borderColor="primary"
     borderWidth={2}
-    alignItems="center"
-    gap={2}
-    paddingHorizontal={5}>
-    <Text variant="b3semiBold" color="primary" textAlign="center">
-      {value}
+    px={5}
+    alignItems="center">
+    <Text color={isSelected ? 'white' : 'primary'} textAlign="center">
+      {value} {label}
     </Text>
-    <Text textAlign="center" color="primary" variant="b3semiBold">
-      {label}
-    </Text>
-  </Card>
+  </Clickable>
 );
 
 export const AccountScreen = () => {
   const {userId, accessToken} = useSelector((state: RootState) => state.user);
   const navigation = useNavigation();
   const {data: user, isLoading: isUserLoading} = useGeUserQuery({userId});
+  const [selectedTab, setSelectedTab] = React.useState<'services' | 'requests'>(
+    'services',
+  );
+
   const {
     data: services,
     isLoading: isServicesLoading,
@@ -126,20 +133,24 @@ export const AccountScreen = () => {
     }, []),
   );
 
-
   if (!accessToken) {
-    return(<Screen>
-      <ContentSafeAreaView alignItems="center" justifyContent="center" flex={1}>
-        <Box g={5}>
-          <Text textAlign="center" color="danger">You are not logged in yet. Please login to continue</Text>
-          <Button
-          onPress={()=>navigation.navigate('Login')}
-          >
-            <Button.Text title="Go to login" />
-          </Button>
-        </Box>
-      </ContentSafeAreaView>
-    </Screen>);
+    return (
+      <Screen>
+        <ContentSafeAreaView
+          alignItems="center"
+          justifyContent="center"
+          flex={1}>
+          <Box g={5}>
+            <Text textAlign="center" color="danger">
+              You are not logged in yet. Please login to continue
+            </Text>
+            <Button onPress={() => navigation.navigate('Login')}>
+              <Button.Text title="Go to login" />
+            </Button>
+          </Box>
+        </ContentSafeAreaView>
+      </Screen>
+    );
   }
 
   if (isUserLoading || isServicesLoading || isRequestsLoading) {
@@ -150,31 +161,72 @@ export const AccountScreen = () => {
     );
   }
 
+  console.log(requests);
+
   return (
     <Screen background="white">
       <ProfileSection user={user} />
       <ContentSafeAreaView flex={1}>
-        <HStack g={5} mt={5} justifyContent="center" px={5}>
-          <StatsCard value={services?.results?.length || 0} label="Services" />
-          <StatsCard value={requests?.results?.length || 0} label="Requests" />
-          <StatsCard value={5} label="Completed" />
-        </HStack>
-        <VStack mt={5} flex={1}>
-          <FlashList
-            ListEmptyComponent={() => (
-              <Text textAlign="center" mt={3} variant="heading3" color="black">
-                No Service Found
-              </Text>
-            )}
-            contentContainerStyle={{paddingTop: 10}}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            data={services?.results}
-            ItemSeparatorComponent={() => <Box mb={5} />}
-            renderItem={({item}) => <PersonalServiceCard service={item} />}
-            keyExtractor={item => item._id ?? item.id}
-            estimatedItemSize={200}
+        <HStack g={5} mt={5}>
+          <StatsCard
+            value={services?.results?.length || 0}
+            label="Services"
+            onPress={() => setSelectedTab('services')}
+            isSelected={selectedTab === 'services'}
           />
+          <StatsCard
+            value={requests?.results?.length || 0}
+            label="Requests"
+            onPress={() => setSelectedTab('requests')}
+            isSelected={selectedTab === 'requests'}
+          />
+        </HStack>
+
+        <VStack mt={5} flex={1}>
+          {selectedTab === 'services' ? (
+            <FlashList
+              ListEmptyComponent={() => (
+                <Text
+                  textAlign="center"
+                  mt={3}
+                  variant="heading3"
+                  color="black">
+                  No Service Found
+                </Text>
+              )}
+              contentContainerStyle={{paddingTop: 10}}
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+              data={services?.results}
+              ItemSeparatorComponent={() => <Box mb={5} />}
+              renderItem={({item}) => <PersonalServiceCard service={item} />}
+              keyExtractor={item => item._id ?? item.id}
+              estimatedItemSize={200}
+            />
+          ) : null}
+
+          {selectedTab === 'requests' ? (
+            <FlashList
+              ListEmptyComponent={() => (
+                <Text
+                  textAlign="center"
+                  mt={3}
+                  variant="heading3"
+                  color="black">
+                  No Requesters found
+                </Text>
+              )}
+              contentContainerStyle={{paddingTop: 10}}
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+              data={requests?.results}
+              extraData={[1]}
+              ItemSeparatorComponent={() => <Box mb={5} />}
+              renderItem={({item}) => <RequestCard request={item} />}
+              keyExtractor={item => item._id ?? item.id}
+              estimatedItemSize={200}
+            />
+          ) : null}
         </VStack>
       </ContentSafeAreaView>
     </Screen>
--- src/screens/authenticated/request/RequestCard.tsx
+++ src/screens/authenticated/request/RequestCard.tsx
@@ -1,14 +1,140 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Box,
+  Card,
+  Center,
+  Divider,
+  FastImage,
+  HStack,
+  IconButton,
+  Text,
+} from '@/components';
+import {getImageUrl} from '@/helper/image';
+import {
+  useCreateChatMutation,
+  useUpdateServiceRequestMutation,
+} from '@/store/apiSlice';
+import {useNavigation} from '@react-navigation/native';
+import moment from 'moment';
 import React from 'react';
+import {Linking} from 'react-native';
 
-const RequestCard = () => {
+const RequestCard = ({request}: any) => {
+  const [createChat, {isLoading}] = useCreateChatMutation();
+  const [updateServiceRequest] = useUpdateServiceRequestMutation();
+  const navigation = useNavigation();
+  const updateStatus = async (status: string, id: string) => {
+    try {
+      const res = await updateServiceRequest({
+        status,
+        id,
+      }).unwrap();
+      // refetch();
+    } catch (e) {
+      toast.error(e?.data?.message || 'Failed to update the service requests');
+    }
+  };
+
+  const handleCreateChat = async () => {
+    try {
+      const data = await createChat({userId: request?.requester?.id}).unwrap();
+      navigation.navigate('Chat', {
+        user: request?.owner?.id,
+        chatId: data?._id,
+      });
+    } catch (error) {
+      console.error('Error creating chat:', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>RequestCard</Text>
-    </View>
+    <Card variant="outlined" paddingHorizontal={3} paddingVertical={4}>
+      <HStack g={5} mt={3}>
+        <Center>
+          <IconButton
+            onPress={() => updateStatus('pending', request?.id)}
+            padding={0}
+            icon="pending"
+            variant="vector"
+            type="material"
+            color={request?.status === 'pending' ? 'secondary' : 'secondary200'}
+          />
+          <Text variant="b5regular">Pending</Text>
+        </Center>
+        <Center>
+          <IconButton
+            onPress={() => updateStatus('rejected', request?.id)}
+            padding={0}
+            icon="cancel"
+            variant="vector"
+            type="material"
+            color={request?.status === 'rejected' ? 'danger' : 'secondary200'}
+          />
+          <Text variant="b5regular">Rejected</Text>
+        </Center>
+        <Center>
+          <IconButton
+            onPress={() => updateStatus('approved', request?.id)}
+            padding={0}
+            icon="check-circle"
+            variant="vector"
+            type="feather"
+            color={request?.status === 'approved' ? 'success' : 'secondary200'}
+          />
+          <Text variant="b5regular">Approved</Text>
+        </Center>
+        <Center>
+          <IconButton
+            onPress={() => updateStatus('completed', request?.id)}
+            padding={0}
+            icon="clipboard-check"
+            variant="vector"
+            type="fa5"
+            color={request?.status === 'completed' ? 'primary' : 'secondary200'}
+          />
+          <Text variant="b5regular">Completed</Text>
+        </Center>
+        <Center>
+          <IconButton
+            onPress={() => updateStatus('completed', request?.id)}
+            padding={0}
+            icon="eye"
+            variant="vector"
+            type="fa"
+            color="primary"
+          />
+          <Text variant="b5regular">View Service</Text>
+        </Center>
+      </HStack>
+      <Divider borderWidth={0.5} />
+      <HStack justifyContent="space-between" py={3}>
+        <HStack g={4}>
+          <FastImage
+            width={30}
+            height={30}
+            borderRadius="rounded-full"
+            source={{uri: getImageUrl(request?.requester?.profilePicture)}}
+          />
+          <Box>
+            <Text>{request?.requester?.name}</Text>
+            <Text variant="b5regular">
+              {moment(request?.createdAt).format('MM Do YY, h:mm a')}
+            </Text>
+          </Box>
+        </HStack>
+        <HStack>
+          <IconButton
+            onPress={() => handleCreateChat()}
+            icon="chatbubble-ellipses-outline"
+            type="ionicon"
+            variant="vector"
+          />
+        </HStack>
+      </HStack>
+      {request?.requestDetails ? (
+        <Text variant="b3regular">{request?.requestDetails}</Text>
+      ) : null}
+    </Card>
   );
 };
 
 export default RequestCard;
-
-const styles = StyleSheet.create({});
